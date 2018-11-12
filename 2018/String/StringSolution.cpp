3f3bb8e97811eae09e9714f579eca3fbc8836e7a
Nandem <nandem@126.com>
2018-11-12 16:00:56 +0800

    颠倒整数

--- 2018/String/StringSolution.cpp
+++ 2018/String/StringSolution.cpp
@@ -4,6 +4,9 @@
 
 #include <vector>
 #include <cstring>
+#include <stdlib.h>
+#include <math.h>
+#include <sstream>
 #include "../../Utils/CommonUtil.h"
 
 using namespace std;
@@ -32,16 +35,54 @@ public:
 
         return s;
     }
+
+    /**
+     * 题目:
+     *     颠倒整数
+     * 描述:
+     *     给定一个 32 位有符号整数，将整数中的数字进行反转。
+     * 说明:
+     *     假设我们的环境只能存储 32 位有符号整数，
+     *     其数值范围是 [−2^31,  2^31 − 1]。根据这个假设，
+     *     如果反转后的整数溢出，则返回 0。
+     * @param x
+     * @return
+     */
+    int reverse(int x)
+    {
+        int sign = x < 0 ? -1 : 1;
+        int ret = 0;
+
+        string xs = to_string(x);
+
+        for (int i = 0; i < xs.size() / 2; ++i)
+        {
+            char tem = xs[i];
+            xs[i] = xs[xs.size() - i - 1];
+            xs[xs.size() - i - 1] = tem;
+        }
+
+        istringstream stream(xs);
+        stream >> ret;
+
+        if (ret >= pow(2, 31) - 1)
+            return 0;
+
+        ret *= sign;
+
+        return ret;
+    }
 };
 
 int main()
 {
     auto *solution = new StringSolution;
 
-    string testCase1 = "A man, a plan, a canal: Panama";
-//    string testCase1 = "nandem";
+//    string testCase = "A man, a plan, a canal: Panama";
+    int testCase = -2147483648;
 
-    cout << solution->reverseString(testCase1) << endl;
+//    cout << solution->reverseString(testCase) << endl;
+    cout << solution->reverse(testCase) << endl;
 
     return 0;
 }
